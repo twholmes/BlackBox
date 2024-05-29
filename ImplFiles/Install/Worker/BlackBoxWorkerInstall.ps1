f3816f0fe5db75c8e470bf1eda68402469c33a96
Trevor William Holmes <39395981+twholmes@users.noreply.github.com>
2024-05-29 21:47:33 +1000

    2024-05-29 > 8

--- ImplFiles/Install/Worker/BlackBoxWorkerInstall.ps1
+++ ImplFiles/Install/Worker/BlackBoxWorkerInstall.ps1
@@ -3,6 +3,42 @@
 ###########################################################################
 
 ###########################################################################
+# Write Script Info
+
+function Write-ScriptInfo() 
+{
+  $wmi_OperatingSystem = Get-CimInstance -Class win32_operatingsystem
+
+  $RAMTotal = [Math]::Round($wmi_OperatingSystem.TotalVisibleMemorySize/1MB)
+  $RAMFree = [Math]::Round($wmi_OperatingSystem.FreePhysicalMemory/1MB)
+  $os = [string]$wmi_OperatingSystem.Caption
+
+  $loggedInUser = "{0}\{1}" -f $($env:UserDomain), $($env:UserName)
+
+  $datetime = Get-Date -format "dd.MM.yyyy hh:mm"
+
+  Log ""    
+  Log "------------ Starting process ($PID) -----------------"
+  Log "Script:             $($ScriptName)"  
+  Log "Computer Name:      $($env:COMPUTERNAME)"
+  Log "RAM:                $RAMTotal GB ($RAMFree GB available)"
+  Log "Operating System:   $os"
+  Log ("Logged in user:     {0}" -f $loggedInUser)  
+  Log ("----------------- {0} --------------------" -f $datetime)
+  Log ""
+}
+
+function WriteScriptInfo
+{ 
+  ## *************************
+  ## SCRIPT INFO
+  ## *************************
+  Write-ScriptInfo
+
+	return $true
+}
+
+###########################################################################
 # Configure Windows Features For BlackBox
 
 function ConfigureWindowsFeaturesForBlackBox
@@ -84,6 +120,167 @@ function ConfigureWindowsFeaturesForBlackBox
 
 function InstallBlackBoxWorker
 {
+  # set key worker app config variables
+	$WorkerApp = GetConfigValue "WorkerApp"
+	$WorkerAppPath = GetConfigValue "WorkerAppPath"
+  
+  # set BlackBox folders
+  Log "***********************"
+  Log "SET BLACKBOX FOLDERS"
+  Log "***********************"
+  Log ""
+
+  # set root path
+  $RootDir = [Environment]::GetEnvironmentVariable('BlackBoxRootDir')
+  if ([string]::IsNullOrEmpty($RootDir) -OR !(Test-Path $RootDir)) 
+  {
+    $RootDir = $WorkerAppPath
+  }
+
+  # set scripts path
+  $ScriptsDir = [Environment]::GetEnvironmentVariable('BlackBoxScriptsDir')
+  if ([string]::IsNullOrEmpty($ScriptsDir) -OR !(Test-Path $ScriptsDir)) 
+  {
+    $ScriptsDir = $RootDir + "\Scripts"
+  }
+  
+  # set temp path
+  $TempDir = [Environment]::GetEnvironmentVariable('BlackBoxTempDir')
+  if ([string]::IsNullOrEmpty($TempDir) -OR !(Test-Path $TempDir)) 
+  {
+    $TempDir = $RootDir + "\temp"
+  }
+  
+  # set tasks path
+  $TasksDir = [Environment]::GetEnvironmentVariable('BlackBoxTasksDir')
+  if ([string]::IsNullOrEmpty($TasksDir) -OR !(Test-Path $TasksDir)) 
+  {
+    $TasksDir = $RootDir + "\Tasks"
+  }
+  
+  # set data path
+  $DataDir = [Environment]::GetEnvironmentVariable('BlackBoxDataDir')
+  if ([string]::IsNullOrEmpty($DataDir) -OR !(Test-Path $DataDir)) 
+  {
+    $DataDir = $RootDir + "\Data"
+  }
+  
+  # set setup path
+  $SetupDir = [Environment]::GetEnvironmentVariable('BlackBoxSetupDir')
+  if ([string]::IsNullOrEmpty($SetupDir) -OR !(Test-Path $SetupDir)) 
+  {
+    $SetupDir = $RootDir + "\Setup"
+  }
+  
+  # set packages path
+  $PackagesDir = [Environment]::GetEnvironmentVariable('BlackBoxPackagesDir')
+  if ([string]::IsNullOrEmpty($PackagesDir) -OR !(Test-Path $PackagesDir)) 
+  {
+    $PackagesDir = $RootDir + "\Packages"
+  }
+  
+  # set woking directory path
+  $WorkingDir = [Environment]::GetEnvironmentVariable('BlackBoxWorkingDir')
+  if ([string]::IsNullOrEmpty($WorkingDir) -OR !(Test-Path $WorkingDir)) 
+  {
+    $WorkingDir = $RootDir + "\Jobs\00000000-0000-0000-0000-000000000000"
+  }
+
+  # Show directories
+  Log ("RootDir:     {0}" -f $RootDir)
+  Log ("ScriptsDir:  {0}" -f $ScriptsDir)
+  Log ("TempDir:     {0}" -f $TempDir)
+  Log ("TasksDir:    {0}" -f $TasksDir)  
+  Log ("DataDir:     {0}" -f $DataDir)
+  Log ("SetupDir:    {0}" -f $SetupDir)  
+  Log ("PackagesDir: {0}" -f $PackagesDir)    
+  Log ("WorkingDir:  {0}" -f $WorkingDir)
+  Log ""  
+
+  # get the archive
+	$WorkerZipFile = GetConfigValue "WorkerZipFile"
+
+  # set the package name
+  $ZipFile = [Environment]::GetEnvironmentVariable('BlackBoxPackageName')
+  if ([string]::IsNullOrEmpty($ZipFile)) 
+  {
+    $ZipFile = $WorkerZipFile
+  }
+
+  # find the package    
+  $ZipFileList = Get-ChildItem -Path $RootDir -Filter $ZipFile -File -Recurse -ErrorAction SilentlyContinue -Force
+  $ZipFileItem = $ZipFileList[0]
+  if ([string]::IsNullOrEmpty($ZipFileItem)) 
+  {
+    Log ("Archive filename not in root folder {0}" -f $RootDir)
+    return $false
+  }
+  $ZipFileFullName = $ZipFileItem.FullName
+  
+  # create a new temp folder for file processing
+  $tempFolderPath = Join-Path $Env:Temp $(New-Guid)
+  ##$tempFolderPath = Join-Path $Env:Temp ([System.IO.Path]::GetRandomFileName())
+  New-Item -Type Directory -Path $tempFolderPath | Out-Null
+  
+  # unzip the archive
+  Log "***********************"
+  Log "UNZIP ARCHIVE"
+  Log "***********************"
+  Log ""
+  Log ("Unzip archive file: {0} to temp" -f $ZipFile)
+  Log ("  from: {0}" -f $ZipFileFullName)
+  Log ("  to: {0}" -f $tempFolderPath)
+  Log ""
+  Expand-Archive -LiteralPath $ZipFileFullName -DestinationPath $tempFolderPath -Force
+  
+  ##Add-Type -AssemblyName System.IO.Compression.FileSystem
+  ##[System.IO.Compression.ZipFile]::ExtractToDirectory($ZipFileFullName, $tempFolderPath, $true)
+  
+  Log ""
+  
+  ## copy files from temp to root subfolders
+  Log "***********************"
+  Log "COPY FILES TO ROOT"
+  Log "***********************"
+  Log ""
+  
+  $sourceRoot = Join-Path -Path $tempFolderPath -ChildPath "root"
+  
+  $source = Join-Path -Path $sourceRoot -ChildPath "Scripts\*"
+  Log ("Copy {0} files from [tempdir]\Scripts to [rootdir]\Scripts" -f (Get-ChildItem $source).Count)
+  Copy-Item -Path $source -Destination $ScriptsDir -Recurse -Force
+  
+  $source = Join-Path -Path $sourceRoot -ChildPath "Tasks\*"
+  Log ("Copy {0} files from [tempdir]\Tasks to [rootdir]\Tasks" -f (Get-ChildItem $source).Count)
+  Copy-Item -Path $source -Destination $TasksDir -Recurse -Force
+  
+  $source = Join-Path -Path $sourceRoot -ChildPath "Packages\*"
+  Log ("Copy {0} files from [tempdir]\Packages to [rootdir]\Packages" -f (Get-ChildItem $source).Count)
+  Copy-Item -Path $source -Destination $PackagesDir -Recurse -Force
+  
+  $source = Join-Path -Path $sourceRoot -ChildPath "Data\*"
+  $dest = Join-Path -Path $RootDir -ChildPath "Data"
+  Log ("Copy {0} files from [tempdir]\Data to [rootdir]\Data" -f (Get-ChildItem $source).Count)
+  Copy-Item -Path $source -Destination $dest -Recurse -Force
+  
+  $source = Join-Path -Path $sourceRoot -ChildPath "Setup\*"
+  $dest = Join-Path -Path $RootDir -ChildPath "Setup"
+  Log ("Copy {0} files from [tempdir]\Setup to [rootdir]\Setup" -f (Get-ChildItem $source).Count)
+  Copy-Item -Path $source -Destination $dest -Recurse -Force
+  
+  Log ""
+  
+  ## remove all remaining extract files
+  Log ("Cleanup temp folder files {0}" -f $tempFolderPath)
+  Get-ChildItem -Path $tempFolderPath -Include *.* -File -Recurse | foreach { $_.Delete()}
+  
+  ## delete any empty directories left behind after deleting the old files
+  Get-ChildItem -Path $tempFolderPath -Recurse -Force | Where-Object { $_.PSIsContainer -and (Get-ChildItem -Path $_.FullName -Recurse -Force | Where-Object { !$_.PSIsContainer }) -eq $null } | Remove-Item -Force -Recurse
+  
+  Log ""
+  
+
+
 	Log "Installation of BlackBox worker app components succeeded"
 	return $true	
 }
