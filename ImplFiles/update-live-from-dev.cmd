0ad36802c6358bc6e72779d2b144e93aa6aae61c
Trevor William Holmes <39395981+twholmes@users.noreply.github.com>
2024-06-11 09:51:48 +1000

    2024-06-11 > 2

---
+++ ImplFiles/update-live-from-dev.cmd
@@ -0,0 +1,13 @@
+@ECHO OFF
+REM Source script to run bbadmin.ps1
+REM Working Folder is C:\BlackBox\ImplFiles
+REM Created at 29/05/2024 11:30
+
+REM Change to directory containing this file
+CD /D %~dp0
+
+REM Run the PowerShell command line as Admin
+PowerShell -NoProfile -ExecutionPolicy Bypass "& .\bbadmin.ps1 UpdateLiveFromDevPublished"
+
+pause
+
